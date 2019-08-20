939783db0feb97c625045cc2754f7fab001e4444
isjulianJaywing <julian.jackson@jaywing.com>
2019-08-20 21:23:57 +0100

    Added decending sort comparison function for sort method

--- test.js
+++ test.js
@@ -1,85 +1,79 @@
-'use strict';
+// This could be moved into the annomous function, as it stands it applies to whole file.
+"use strict";
 
 //Class to store and sort a list of numeric years (4 digit)
-var YearData = (function () {
+var YearData = (function() {
+  //Constructor
+  function YearData() {
+    this.yearDataList = [];
+  }
 
-    //Constructor
-    function YearData() {
-        this.yearDataList = [];
+  //Add either an array of year or a single year to the associative array or year strings
+  YearData.prototype.addToYearList = function(yearToAdd) {
+    var _this = this;
+    var i, inLength;
+
+    if (yearToAdd instanceof Array) {
+      inLength = yearToAdd.length;
+      //FIXME: the below sort would convert the array items into strings and then sort. Removed.
+      yearToAdd.sort(decendingSort);
+
+      for (i = 0; i < inLength; i++) {
+        if (_this.yearDataList.indexOf(yearToAdd[i]) < 0) {
+          //Not in list yet, so add to start of list
+          _this.yearDataList.push(yearToAdd[i]);
+        }
+      }
+    } else {
+      _this.yearDataList.push(yearToAdd);
     }
 
-    //Add either an array of year or a single year to the associative array or year strings
-    YearData.prototype.addToYearList = function (yearToAdd) {
+    //Make sure everything is an integer
+    for (i = 0; i < _this.yearDataList.length; i++) {
+      _this.yearDataList[i] = parseInt(_this.yearDataList[i]);
+    }
 
-        var _this = this;
-        var i, inLength;
+    //Sort the list using a number compare
+    // _this.yearDataList.sort(decendingSort);
 
-        if (yearToAdd instanceof Array) {
+    return _this.yearDataList;
+  };
 
-            inLength = yearToAdd.length;
-            _this.yearToAdd.sort();
-            for (i = 0; i < inLength; i++) {
-                if (_this.yearDataList.indexOf(yearToAdd[i]) < 0) {
-                    //Not in list yet, so add to start of list
-                    _this.yearDataList.push(yearToAdd[i]);
+  //Return a copy of the array
+  YearData.prototype.getYearDataList = function() {
+    return this.yearDataList.slice();
+  };
 
-                }
-            }
-        } else {
-            _this.yearDataList.push(yearToAdd);
-        }
+  //Use the underlying year data list to return a contiguous list of years up until this year
+  //ie. fill in gaps and add in next year
+  YearData.prototype.getContiguousYearDataListWithExtension = function() {
+    var retArray = [];
+    var i, minYear, maxYear;
+    var timeNow = new Date();
+    var nextYear = timeNow.getFullYear() + 1;
 
-        //Make sure everything is an integer
-        for (i = 0; i < _this.yearDataList.length; i++) {
-            _this.yearDataList[i] = parseInt(_this.yearDataList[i]);
-        }
+    minYear = Math.min.apply(Math, this.yearDataList);
+    maxYear = Math.max.apply(Math, this.yearDataList);
 
-        //Sort the list using a number compare
-        _this.yearDataList.sort(function (a, b) {
-            if (a < b) {
-                return -1;
-            } else if (a > b) {
-                return 1;
-            }
-            return 0;
-        });
+    //Years are in descending order
+    if (nextYear > maxYear) {
+      maxYear = nextYear;
+    }
+    if (minYear > 0 && maxYear - minYear < 100) {
+      for (i = maxYear; i >= minYear; i--) {
+        retArray.push(i);
+      }
+    }
 
+    //We must sort the return list
+    return retArray.sort(decendingSort);
+  };
 
-        return _this.yearDataList;
-    };
+  // Errors from using a Array.sort() method without passing a comparison function lead the array items being converted to a string and having it sorted by utf units
+  // this comparison function passed into the the sort methods above; decending the order.
+  function decendingSort(a, b) {
+    return b - a;
+  }
 
-
-    //Return a copy of the array
-    YearData.prototype.getYearDataList = function () {
-        return this.yearDataList.slice();
-    };
-
-
-    //Use the underlying year data list to return a contiguous list of years up until this year
-    //ie. fill in gaps and add in next year
-    YearData.prototype.getContiguousYearDataListWithExtension = function () {
-        var retArray = [];
-        var i, minYear, maxYear;
-        var timeNow = new Date();
-        var nextYear = timeNow.getFullYear() + 1;
-
-        minYear = Math.min.apply(Math, this.yearDataList);
-        maxYear = Math.max.apply(Math, this.yearDataList);
-
-        //Years are in descending order
-        if (nextYear > maxYear) {
-            maxYear = nextYear;
-        }
-        if (minYear > 0 && maxYear - minYear < 100) {
-            for (i = maxYear; i >= minYear; i--) {
-                retArray.push(i);
-            }
-        }
-
-        //We must sort the return list
-        return retArray.sort(
-        );
-    };
-
-    return YearData;
-}());
+  return YearData;
+})();
